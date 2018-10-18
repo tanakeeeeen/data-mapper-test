322a5ee588cd8986f2994164c04ab2e910b11727
tanakeeeeen <Kensuke.Tanaka@sony.com>
2018-10-18 10:35:53 +0900

    First commit

---
+++ handler.ts
@@ -0,0 +1,13 @@
+import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
+
+export const hello: Handler = async (event: APIGatewayEvent, context: Context) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  return response
+}
